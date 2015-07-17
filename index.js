bbed2c6b0c32e6fbc75d992e63e781318b920f07
MoA <honomoa@gmail.com>
2015-07-17 11:18:22 +0800

    add whileQ, whileTureQ, mapSeries

---
+++ index.js
@@ -0,0 +1,9 @@
+var whileQ = require('./lib/whileQ');
+var whileTrueQ = require('./lib/whileTrueQ');
+var mapSeries = require('./lib/mapSeries');
+
+module.exports = {
+  whileQ: whileQ,
+  whileTrueQ: whileTrueQ,
+  mapSeries: mapSeries
+};
